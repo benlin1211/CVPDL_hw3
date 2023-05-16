698bbe17514293b031e2f5a10cab69d6d3b2a92e
benlin1211 <ben1211.eecs06@nctu.edu.tw>
2023-05-16 15:14:38 +0000

    R-yolo

---
+++ download_data.sh
@@ -0,0 +1,10 @@
+# Download dataset
+gdown 1F1N_YgB_43Z-IQOwocmsjnBL3AjycrkC -O hw3_data.zip
+
+# Unzip the downloaded zip file
+mkdir hw3_data
+unzip ./hw3_data.zip -d hw3_data
+rm -rf ./hw3_data.zip
+
+# Download vgg pre-trained model to prepare the data of translation image
+gdown 199luoCcfhAF_8kydAwziOIPVqyiLECbN -O ./R-YOLO/runs/vgg16_caffe.pth
