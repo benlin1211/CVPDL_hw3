78d5e94413edd36565ae1cd9781350d0270fa6a3
benlin1211 <ben1211.eecs06@nctu.edu.tw>
2023-05-23 09:00:22 +0000

    all basic done

--- download_data.sh
+++ download_data.sh
@@ -8,8 +8,8 @@ mkdir hw3_data
 unzip ./hw3_data.zip -d hw3_data
 # rm -rf ./hw3_data.zip
 
-# Download vgg pre-trained model to prepare the data of translation image
-gdown 199luoCcfhAF_8kydAwziOIPVqyiLECbN -O ./R-YOLO/runs/vgg16_caffe.pth
+# # Download vgg pre-trained model to prepare the data of translation image
+# gdown 199luoCcfhAF_8kydAwziOIPVqyiLECbN -O ./R-YOLO/runs/vgg16_caffe.pth
 
 
 ## bash download_data.sh
